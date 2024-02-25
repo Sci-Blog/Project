f03cec059090d4f7b3d85289e5301777dc2ea617
Sci-Blog <153494887+Sci-Blog@users.noreply.github.com>
2024-02-25 12:37:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+//Docs Buttons
+function astronomy(){
+    window.location="astronomy.html";
+}
+function biology(){
+    window.location="biology.html";
+}
+function physics(){
+    window.location="physics.html";
+}
+function chemistry(){
+    window.location="chemistry.html";
+}
+function psychology(){
+    window.location="psychology.html";
+}
+function mathematics(){
+    window.location="mathematics.html";
+}
+function geology(){
+    window.location="geology.html";
+}
+function minifacts(){
+    window.location="minifacts.html";
+}
+function debate(){
+    window.location="debate.html";
+}
+function lessons(){
+    window.location="lessons.html";
+}
+
+//Nav Bar Mobile Buttons
+function docs(){
+    window.location="docs.html"
+}
+function about(){
+    window.location="about.html"
+}
+//WORK MORE ON THIS!!!!
